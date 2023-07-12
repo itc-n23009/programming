9883ab786557cfd1659a7a09d69aa50e345b5378
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-07-12 06:31:54 +0000

    Q3_3_1.py

--- CHAPTER03/Q3_3_1.py
+++ CHAPTER03/Q3_3_1.py
@@ -1,6 +1,3 @@
 my_list = ["tokyo", "osaka", "fukuoka", "aichi", "kyoto", "chiba", "saitama", "gunma"]
-my_list_6 = []
-for s in my_list:
-    if len(s) >= 6:
-        my_list_6.append(s)
+my_list_6 = [s for s in my_list if len(s) >= 6]
 print(my_list_6)
