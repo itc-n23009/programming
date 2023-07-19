1135781705195d9d921bfedb72496c4d9da49455
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-07-19 07:10:56 +0000

    Q4_2_2.py

---
+++ CHAPTER04/Q4_2_2.py
@@ -0,0 +1,10 @@
+def perrin(m=100):
+    '''
+    ペラン数列 : 3, 0, 2, 3, 2, 5, 5, 7, 10, 12, 17, 22, 29, 39, ...
+    '''
+    a, b, c = 3, 0, 2
+    result = []
+    while a < m:
+        result.append(a)
+        a, b, c = b, c, a+b
+    return result
