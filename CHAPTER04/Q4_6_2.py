5ff3a18bf54a2ebe312b67d87ffd1f51048b14a6
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-07-20 05:43:51 +0000

    Q4_6_2.py

---
+++ CHAPTER04/Q4_6_2.py
@@ -0,0 +1,5 @@
+students_by_height = sorted(students_data, key=lambda s: s[1])
+students_by_weight = sorted(students_data, key=lambda s: s[2])
+print("\nSort by height")
+for student in students_by_height:
+    print(student)
