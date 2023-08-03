ab90bb57165e3bd3345eb8666f3d6123949ed6ad
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-08-03 07:38:23 +0000

    Q4_8_1.py

--- CHAPTER04/Q4_8_1.py
+++ CHAPTER04/Q4_8_1.py
@@ -4,4 +4,8 @@ cal = calendar.TextCalendar(calendar.SUNDAY)
 year = 2023
 month = 8
 cal_str = cal.formatmonth(year, month)
+cal_str = cal_str.replace("Su", "\033[31mSu\033[0m")
+cal_str = cal_str.replace("Sa", "\033[34mSa\033[0m")
+
+
 print(cal_str)
