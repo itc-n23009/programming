36dd21eb8b1955f5ce96d08298b855644eaa4133
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-08-08 05:52:04 +0000

    Q5_1_2.py

--- CHAPTER05/Q5_1_2.py
+++ CHAPTER05/Q5_1_2.py
@@ -1,7 +1,5 @@
 stack = []
-stack.append(1)
-print("stack:", stack)
-stack.append(2)
+stack += [1, 2]
 print("stack:", stack)
 print("pop 1st value:", stack.pop())
 print("pop 2nd value:", stack.pop())
