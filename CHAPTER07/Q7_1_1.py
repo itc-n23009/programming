b246f4f535e77b5580f8fd5e35d1b68fe37e41e4
Kamiya Yuta <n23009@std.it-college.ac.jp>
2023-08-22 05:14:40 +0000

    Q7_1_1.py

--- CHAPTER07/Q7_1_1.py
+++ CHAPTER07/Q7_1_1.py
@@ -2,10 +2,7 @@ name_age = {"tanaka": 35, "satou": 25, "suzuki": 27}
 
 
 def dict_info(dict_tbl, key):
-    try:
-        return dict_tbl[key]
-    except:
-        return "key is not found"
+    return dict_tbl.get(key, "key is not found")
 
 
 print(dict_info(name_age, "satou"))
